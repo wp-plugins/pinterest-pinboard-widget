84313a8494a73f22942eddc6a43c55905fa2ff6a
codefish <codefish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-14 08:02:36 +0000

    update version in branch + tag
    git-svn-id: https://plugins.svn.wordpress.org/pinterest-pinboard-widget/tags/1.0.3@652320 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pinterest-pinboard-widget.php
+++ pinterest-pinboard-widget.php
@@ -6,7 +6,7 @@ Plugin URI: http://wordpress.org/extend/plugins/pinterest-pinboard-widget/
 Description: Add a Pinterest Pinboard widget to WordPress.
 Author: CodeFish
 Author URI: http://www.codefish.nl
-Version: 1.0.2
+Version: 1.0.3
 */
 
 /*  Copyright 2012 CodeFish (email: info at codefish.nl)
